330dff28be67a7085a2c6eb3d106d589f56f5cb5
matu.dev <mati.a.gimenez2002@gmail.com>
2025-10-16 20:15:55 -0300

    Auth middleware

--- src/repositories/workspace.repository.js
+++ src/repositories/workspace.repository.js
@@ -1,4 +1,17 @@
+import pool from "../config/mysql.config.js"
 import Workspaces from "../models/Workspace.model.js"
+
+const WORKSPACE_TABLE = {
+    NAME: 'Workspaces',
+    COLUMNS: {
+        ID: '_id',
+        NAME: 'name',
+        URL_IMAGE: 'url_image',
+        ACTIVE: 'active',
+        CREATED_AT: 'created_at',
+        MODIFIED_AT: 'modified_at'
+    }
+}
 
 class WorkspacesRepository {
 
@@ -12,36 +25,76 @@ class WorkspacesRepository {
         })
         return true
         
-    } */ 
-    static async getAll (){
+    } */
+    //Pasar a mysql
+    static async getAll() {
+
+        const query = `SELECT * FROM ${WORKSPACE_TABLE.NAME}`
+        const [result] = await pool.execute(query)
+        return result
+    }
+    /* static async getAll (){
+
+
         const workspaces_get = await Workspaces.find()
 
         return workspaces_get
+    } */
+
+    /*  static async getById (workspaces_id){
+         const workspaces_found = await Workspaces.findById(workspaces_id)
+         return workspaces_found
+     } */
+
+    static async getById(workspace_id) {
+        const query = `
+            SELECT * FROM ${WORKSPACE_TABLE.NAME} WHERE ${WORKSPACE_TABLE.COLUMNS.ID} = ?
+        `;
+        const [result] = await pool.execute(query, [workspace_id]);
+        const workspace_found = result[0];
+        if (!workspace_found) {
+            return null;
+        }
+        return workspace_found;
     }
 
-    static async getById (workspaces_id){
-        const workspaces_found = await Workspaces.findById(workspaces_id)
-        return workspaces_found
-    }
-    static async deleteById(workspaces_id){
-        await Workspaces.findByIdAndDelete(workspaces_id)
-        return true
+    /*  static async deleteById(workspaces_id) {
+         await Workspaces.findByIdAndDelete(workspaces_id)
+         return true
+     } */
+
+    static async deleteById(workspace_id) {
+        const query = `DELETE FROM ${WORKSPACE_TABLE.NAME} WHERE ${WORKSPACE_TABLE.COLUMNS.ID} = ?`;
+        const [result] = await pool.execute(query, [workspace_id]);
+        return result.affectedRows > 0;
     }
 
-    static async updateById(
-        workspaces_id, 
+    /* static async updateById(
+        workspaces_id,
         new_values
-    ){
+    ) {
         const workspace_updated = await Workspaces.findByIdAndUpdate(
-            workspaces_id, 
-            new_values, 
+            workspaces_id,
+            new_values,
             {
                 new: true
             }
         )
         return workspace_updated
     }
+ */
 
+    static async updateById(workspace_id, new_values) {
+        const update_fields = Object.keys(new_values);
+        const fields_query = update_fields
+            .map((field) => `${field} = ?`)
+            .join(" , ");
+        const values = Object.values(new_values);
+        const query = `UPDATE ${WORKSPACE_TABLE.NAME} SET ${fields_query} WHERE ${WORKSPACE_TABLE.COLUMNS.ID} = ? `;
+        await pool.execute(query, [...values, workspace_id]);
+        const workspace_updated = await this.getById(workspace_id);
+        return workspace_updated;
+    }
 }
 
 export default WorkspacesRepository
--- src/routes/workspace.route.js
+++ src/routes/workspace.route.js
@@ -3,21 +3,22 @@ import WorkspacesRepository from '../repositories/workspace.repository.js'
 import { validarId } from '../utils/validations.utils.js'
 import { ServerError } from '../utils/customError.utils.js'
 import WorkspaceController from '../controllers/workspace.controller.js'
+import authMiddleware from '../middleware/auth.middleware.js'
 
 //Manejar consultas referidas a workspace
 
 const workspace_router = express.Router()
 
 
-workspace_router.get('/', WorkspaceController.getAll )
+workspace_router.get('/', authMiddleware, WorkspaceController.getAll )
 
 
-workspace_router.get('/:workspace_id', WorkspaceController.getById )
+workspace_router.get('/:workspace_id', authMiddleware, WorkspaceController.getById )
 
 //Crear el WorkspaceController con los metodos .post, .getById, getAll
 
 //Este es el endpoint para crear workspaces
-workspace_router.post('/', WorkspaceController.post)
+workspace_router.post('/', authMiddleware, WorkspaceController.post)
 
 
 
